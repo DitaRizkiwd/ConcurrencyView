68f6637e1fff3215cae8c8d32cad928f5b578c5f
DitaRizkiwd <rdita201@gmail.com>
2024-04-22 08:45:41 +0700

    Initial Commit

---
+++ ConcurrencyView/ConcurrencyViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcurrencyViewApp.swift
+//  ConcurrencyView
+//
+//  Created by MACBOOK PRO on 22/04/24.
+//
+
+import SwiftUI
+
+@main
+struct ConcurrencyViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
